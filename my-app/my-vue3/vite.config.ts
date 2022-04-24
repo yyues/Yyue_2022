f9d689fe6dc1ffb85cc86c68245032f78ae9f4b9
yyues <938778575@qq.com>
2022-04-24 18:46:53 +0800

    Element AutoImportAndStyle

--- my-app/my-vue3/vite.config.ts
+++ my-app/my-vue3/vite.config.ts
@@ -4,13 +4,43 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 // Gzip
 import viteCompression from 'vite-plugin-compression'
+// Element-UI AutoImport Setting
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// Element-UI CSS Setting
+import ElementPlus from 'unplugin-element-plus/vite'
 
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), VitePWA(), viteCompression()],
+  plugins: [
+    vue(),
+    VitePWA(),
+    viteCompression(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass'
+          // directives: true,
+          // version: "2.1.5",
+        })
+      ]
+    }),
+    ElementPlus()
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "/@/styles/element/index.scss" as *;`
+      }
+    }
+  },
   resolve: {
     // 配置路径别名
     alias: {

046e083887f51fce56ce610f3b2c5fd8883278fe
yyues <938778575@qq.com>
2022-04-24 11:06:18 +0800

    vue -/@别名配置

--- my-app/my-vue3/vite.config.ts
+++ my-app/my-vue3/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import { VitePWA } from 'vite-plugin-pwa'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: './',
+  plugins: [
+    vue() //VitePWA()
+  ],
+  resolve: {
+    // 配置路径别名
+    alias: {
+      '/@': path.resolve(__dirname, './src')
+    }
+  }
 })

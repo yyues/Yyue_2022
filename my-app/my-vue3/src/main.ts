df01b73b79df8392e009daa97cf3e2efb254d4f3
yyues <938778575@qq.com>
2022-04-24 18:46:29 +0800

    Element-UI Style And base Style

--- my-app/my-vue3/src/main.ts
+++ my-app/my-vue3/src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// App router
 import router from './router/router'
+// App Base Style
+import '/@/styles/base/index.scss'
 
 const app = createApp(App)
 
